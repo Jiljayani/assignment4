9f0e7977cd57cac3073f3d16520e551a171d8f98
Jiljayani <130297509+Jiljayani@users.noreply.github.com>
2023-04-09 10:22:31 -0400

    Add files via upload

---
+++ assignment4/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/main.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- <section class="login_form">
+        <div class="login_box_glass">
+            <form action="POST">
+                <input type="text" name="name" placeholder="Enter you name">
+            </form>
+        </div>
+    </section> -->
+
+
+    <section class="move_gallery">
+        <div class="move_img">
+            <div class="box_wrapper">
+                <div class="box">
+                    <form action="POST">
+                        <input type="text" name="name" placeholder="Enter you name">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
